8af492228a6977d57ac778e4470992a88ddb5910
Davi Braz <davisouzabraz@gmail.com>
2022-05-24 00:36:03 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int n;
+int maior = 0;
+int posicao = 0;
+for (int i = 1; i <= 100; i++)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > maior)
+    {
+        maior = n ;
+        posicao = i ;
+    }
+}
+Console.WriteLine(maior);
+Console.WriteLine(posicao);
+
+Console.ReadLine();
